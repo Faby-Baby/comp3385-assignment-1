45452c5103132bcc1246ffe2dc90df298612bdd6
Faby-Baby <fabianbertrand94@gmail.com>
2024-02-12 22:26:17 -0400

    Styling

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <img src="{{ url('/images/ListCheck.jpeg') }}" alt="Fabian checking a list" srcset="">
     <h2>Fabian Bertrand</h2>
-    <p>About me</p>
+    <p>My name is Fabian. My github username is Faby-Baby. I was born in the year of the dog. I like computers and enjoy programming</p>
 @endsection
